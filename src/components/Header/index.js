9452231daeed52fd340dd1492d876ff4ee1146b9
JmsMaupin1 <jmsMaupin1@yahoo.com>
2017-11-29 21:05:49 -0500

    Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,22 +15,33 @@ export default class index extends Component {
                             <ul className="nav navbar-nav navbar-right">
                                 <MenuChoice target="/" mainText="HOME PAGE" subText="Check out our home page"/>
                                 <MenuChoice target="contact"  mainText="CONTACT" subText="Feel free to contact us"/>
-                                <li><a href="https://app.acuityscheduling.com/schedule.php?owner=13546773">SCHEDULE<i className="fa fa-folder-open-o"></i><span>Schedule an appointment today</span></a></li>
-                                <a 
-                                    href="https://finallypestcontrolllc.acuityscheduling.com/schedule.php?appointmentType=3499984" 
-                                    target="_blank" class="acuity-embed-button" 
-                                    style={{
-                                        background: "#469d6e",
-                                        color: '#fff',
+                                <li><a href="https://app.acuityscheduling.com/schedule.php?owner=13546773">SCHEDULE<i className="fa fa-folder-open-o"></i><span>Non-DCS schedule here</span></a></li>
+                                <li>
+                                    <a 
+                                        href="https://finallypestcontrolllc.acuityscheduling.com/schedule.php?appointmentType=3499984" 
+                                        target="_blank"
+                                        style={{
+                                         background: "#469d6e",
+                                         color: '#fff',
                                         padding: '8px 12px',
-                                        border: '0px',
-                                        boxShadow: '0 -2px 0 rgba(0,0,0,0.15) inset',
-                                        borderRadius: "4px",
-                                        textDecoration: "none",
-                                        display: "inline-block"}}>
-                                    Schedule Appointment</a>
+                                         border: '0px',
+                                         boxShadow: '0 -2px 0 rgba(0,0,0,0.15) inset',
+                                         borderRadius: "4px",
+                                         textDecoration: "none",
+                                         display: "inline-block"}}>
+                                        Schedule Appointment
+                                    </a>
+                                    <br/>
+                                    <span>
+                                    <a href="https://finallypestcontrolllc.acuityscheduling.com/schedule.php?appointmentType=3499984" style={{color: "white"}}>
+                                        DCS Scheduling
+                                    </a>
+                                    </span>
+                                </li>
                             </ul>
                         </div>
+
+                    
                     </div>
                 </div>
                 <SocialLinksBar 
@@ -41,6 +52,7 @@ export default class index extends Component {
                     linkedinURL=""
                     pinterestURL=""
                 />
+
             </div>
 		);
 	}
