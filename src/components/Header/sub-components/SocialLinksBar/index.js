80dbbbd5739aa0991758b224f04602aa9dbe7615
JmsMaupin1 <jmsMaupin1@yahoo.com>
2017-12-19 16:29:16 -0500

    Update Links, update logo, add number in mobile made email and number links

--- src/components/Header/sub-components/SocialLinksBar/index.js
+++ src/components/Header/sub-components/SocialLinksBar/index.js
@@ -4,18 +4,22 @@ export default class index extends Component {
 	render() {
 		return (
 			<div className="nav-social">
-                <i className="fa fa-globe "></i>E-mail:  {this.props.email}  |  <i className="fa fa-mobile "></i>Call: : {this.props.phoneno}  |  &nbsp;
+                <i className="fa fa-globe "></i>E-mail:  <a href={"mailto:" + this.props.email}>{this.props.email}</a>  |  <i className="fa fa-mobile "></i>Call: : <a href={"tel:"+this.props.phone}>{this.props.phone}</a>  |  &nbsp;
 
                 <a href={this.props.facebookURL}>
                   <i className="fa fa-facebook-square "></i>
                 </a>
-                <a href={this.props.linkedinURL}>
+                <a href={this.props.linkedinURL} style={{display: this.props.linkedinURL == "" ? "none" : "inline"}}>
                     <i className="fa fa-linkedin-square "></i>
                 </a>
-                <a href={this.props.pinterestURL}>
+                <a href={this.props.pinterestURL} style={{display: this.props.linkedinURL == "" ? "none" : "inline"}}>
                     <i className="fa fa-pinterest-square "></i>
+                </a>
+                <a href={this.props.pinterestURL} style={{display: this.props.youtubeURL == "" ? "none" : "inline"}}>
+                    <i className="fa fa-youtube-square "></i>
                 </a>
             </div>
 		);
 	}
 }
+//<a href="tel:+1-317-660-8794">+1-317-660-8794</a>
