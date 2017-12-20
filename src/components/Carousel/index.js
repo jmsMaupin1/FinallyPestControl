7e0b0129ff9d3b8031c74f353f75d6c23db75608
JmsMaupin1 <jmsMaupin1@yahoo.com>
2017-12-19 23:25:41 -0500

    Create Banner with testimonials and contact form, fix compile warnings

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -7,14 +7,16 @@ export default class Carousel extends Component {
 		<div className={this.props.classes}>
 			{this.props.title?<h1>{this.props.title}</h1>:null}
 
-    		<div id={this.props.target} className="carousel slide">
+    		<div id={this.props.target} className="carousel slide" data-ride="carousel">
 
            		<div className="carousel-inner">
            			{this.props.children}
             	</div>
 
 	            <ol className="carousel-indicators">
-	            	
+	            	{this.props.children.map((cur , i)=>{
+	            		return (<li key={i} data-target={"#" + this.props.target} data-slide-to={i} className={i===0?"active":""}></li>)
+	            	})}
 	            </ol>
 
     		</div>
