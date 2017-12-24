f92178794e3826a5de39b01518779325f51dbb7d
JmsMaupin1 <jmsMaupin1@yahoo.com>
2017-12-24 01:31:52 -0500

    Change footer to contact form

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,52 +1,58 @@
 import React, { Component } from 'react';
-import './footer.css';
+import ContactForm from '../ContactForm';
+import './footer.css'
 
 export default class Footer extends Component {
+	constructor(props) {
+	    super(props)
+
+	    this.state = {
+	      height: 0
+	    }
+	}
+
+	componentDidMount() {
+    	const height = this.divElement.clientHeight;
+    	this.setState({ height });
+  	}
+
 	render() {
 		return (
-			<div className="footer">
-				<div className="container">
-		            <div className="row">
-		                <div className="col-lg-8 col-md-8 about-ftr">
-		                    <i className="fa fa-building fa-2x"></i>
-		                    <span>A Small Introduction</span>
-		                    <small>Bee Green Lawn Care</small>
-		                    <p>
-                                At Bee Green, we stand by our service. Customer service is our top priority. It is something we take very serious because if our customers are not satisfied, we are not satisfied. Our work is guaranteed. We provide FREE service calls between treatments for any reason. Our professional technicians are trained to detect and address any concerns. Since 2001, we have been providing lawn service in Carmel IN and would love to show what we can do for you.
-
-                                Bee Green has also received the following honors for our work:
-                                  • Angie’s List Super Service Award 2015
-                                  • Angie’s List Super Service Award 2016
-                                  • Haverstick Lawn of the Month – We have won this accolade a few times on a few different properties
-		                    </p>
-		                </div>
-
-		                {/*<div className="col-lg-4 col-md-4">
-		                		                    <i className="fa fa-paper-plane-o fa-2x"></i>
-		                		                    <span>From the blog</span>
-		                		                    <small>TBA</small>
-		                		                    <div className="blog-footer-div">
-		                		                    	<BlogLink image="assets/img/user2.png" articleLink="#" date="Posted 17 June 2014" text="Pellentesque habitant morbi tristique" />
-		                		                        <BlogLink image="assets/img/user.gif" articleLink="#" date="Posted 27 June 2014" text="Pellentesque habitant morbi tristique" />
-		                		                    </div>
-		                		                </div>*/}
-		               	<div className="col-lg-4 col-md-4"></div>
-
-		                <div className="col-lg-4 col-md-4">
-		                    <i className="fa fa-sliders fa-2x"></i>
-		                    <span>Contact Us!</span>
-		                    <h3>Call: 1-317-660-8794</h3>
-		                    	<a href="mailto:beegreenlawncare@gmail.com">
-								<h3>beegreengrass@gmail.com</h3>
-								</a>
-							<span>Hours: Mon - Fri: 9:00 am - 5:00 pm</span>
-
-							<h2>Get ahold of us today</h2>
-
-		                </div>
-		            </div>
-		        </div>
-	        </div>
+			<div className="plax"
+				 ref={(divElement) => this.divElement = divElement}>
+				<div style={{position: "absolute", width: "100%", height: this.state.height, backgroundColor: "rgba(0, 0, 0, .7)"}} />
+				<div className="col-lg-12" style={{paddingTop: "4%", paddingBottom: "2%"}}>
+					<h1>Contact Us!</h1>
+				</div>
+				<div>
+					<div className="col-lg-6 col-md-6">
+						<div className="col-lg-11 col-lg-offset-1">
+							<h1 style={{paddingBottom: "5%"}}>About Us!</h1>
+							<div style={{textAlign: "left"}}>
+								<p>
+									At Bee Green, we stand by our service. Customer service is our top priority. It is 
+									something we take very serious because if our customers are not satisfied, 
+									we are not satisfied. Our work is guaranteed. We provide FREE service calls 
+									between treatments for any reason. Our professional technicians are trained to detect and
+									address any concerns. Since 2001, we have been providing lawn service in Carmel IN and would 
+									love to show what we can do for you. Bee Green has also received the following honors for our 
+									work: 
+									<br/>
+									<br/>
+									• Angie’s List Super Service Award 2015
+									<br/> 
+									• Angie’s List Super Service Award 2016 
+									<br/>
+									• Haverstick Lawn of the Month – We have won this accolade a few times on a few different properties
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-offset-1 col-lg-4 col-md-4">
+						<ContactForm />
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
