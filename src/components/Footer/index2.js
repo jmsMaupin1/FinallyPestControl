81f20b681270fd6eef27aa18e60fa55b5782abc9
JmsMaupin1 <jmsMaupin1@yahoo.com>
2017-12-26 03:56:24 -0500

    Redirected from Contact page to contact in the footer

--- src/components/Footer/index2.js
+++ src/components/Footer/index2.js
@@ -6,37 +6,39 @@ import ContactForm from '../ContactForm'
 export default class Footer extends Component {
 	render() {
 		return (
-			<Banner image={BgImage} color={[0,0,0]} opacity={.7}>
-				<div className="col-lg-12" style={{padding: "4% 0 2% 0"}}>
-					<h1>Contact Us!</h1>
-				</div>
-				<div className="col-lg-7 col-md-7">
-					<div className="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
-						<h1 style={{paddingBottom: "5%"}}>About Us!</h1>
-						<div style={{textAlign: "left"}}>
-							<p>
-								At Bee Green, we stand by our service. Customer service is our top priority. It is 
-								something we take very serious because if our customers are not satisfied, 
-								we are not satisfied. Our work is guaranteed. We provide FREE service calls 
-								between treatments for any reason. Our professional technicians are trained to detect and
-								address any concerns. Since 2001, we have been providing lawn service in Carmel IN and would 
-								love to show what we can do for you. Bee Green has also received the following honors for our 
-								work: 
-								<br/>
-								<br/>
-								• Angie’s List Super Service Award 2015
-								<br/> 
-								• Angie’s List Super Service Award 2016 
-								<br/>
-								• Haverstick Lawn of the Month – We have won this accolade a few times on a few different properties
-							</p>
+			<div id="footer">
+				<Banner image={BgImage} color={[0,0,0]} opacity={.7}>
+					<div className="col-lg-12" style={{padding: "4% 0 2% 0"}}>
+						<h1>Contact Us!</h1>
+					</div>
+					<div className="col-lg-7 col-md-7">
+						<div className="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+							<h1 style={{paddingBottom: "5%"}}>About Us!</h1>
+							<div style={{textAlign: "left"}}>
+								<p>
+									At Bee Green, we stand by our service. Customer service is our top priority. It is 
+									something we take very serious because if our customers are not satisfied, 
+									we are not satisfied. Our work is guaranteed. We provide FREE service calls 
+									between treatments for any reason. Our professional technicians are trained to detect and
+									address any concerns. Since 2001, we have been providing lawn service in Carmel IN and would 
+									love to show what we can do for you. Bee Green has also received the following honors for our 
+									work: 
+									<br/>
+									<br/>
+									• Angie’s List Super Service Award 2015
+									<br/> 
+									• Angie’s List Super Service Award 2016 
+									<br/>
+									• Haverstick Lawn of the Month – We have won this accolade a few times on a few different properties
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className="col-lg-4 col-md-4">
-					<ContactForm />
-				</div>
-			</Banner>
+					<div className="col-lg-4 col-md-4">
+						<ContactForm />
+					</div>
+				</Banner>
+			</div>
 		);
 	}
 }
--- src/components/Header/sub-components/MenuChoice/index.js
+++ src/components/Header/sub-components/MenuChoice/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router'
 
 export default class index extends Component {
-	render() {
+	render2() {
 		return (
 			<li>
                 <Link to={this.props.target?this.props.target:"#"}>{this.props.mainText}<i className="fa fa-folder-open-o"></i>
@@ -10,5 +10,31 @@ export default class index extends Component {
                 </Link>
             </li>
 		);
+	}
+
+	render() {
+		if(!(this.props.target || this.props.anchor))
+			return <div/>
+
+		else {
+			if(this.props.target){
+				return (
+					<li>
+		                <Link to={this.props.target}>{this.props.mainText}<i className="fa fa-folder-open-o"></i>
+		                    <span>{this.props.subText}</span>
+		                </Link>
+		            </li>
+				);
+			} else {
+				return (
+					<li>
+						<a href={"#" + this.props.anchor}>{this.props.mainText}<i className="fa fa-folder-open-o"/>
+							<span>{this.props.subText}</span>
+						</a>
+					</li>
+				);
+			}
+		}
+
 	}
 }
